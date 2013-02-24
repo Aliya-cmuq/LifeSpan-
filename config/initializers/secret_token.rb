bae5e0a61212af9b1406ff2f4d20057d1dc4dbe9
Aliya <aliya.hashim11@gmail.com>
2013-02-24 16:58:43 +0300

    Starting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeSpan::Application.config.secret_token = 'ef9883151839e98d90676d3d467bc695faa86efdb843506fced3900a0fb79de8ad527a0a1533c9ca49b6739f156d398d29b89f7b779ed6462a648ca72a107902'
